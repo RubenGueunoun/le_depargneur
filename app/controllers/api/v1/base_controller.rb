5b7a0536231349d7f2af7c5b5df2edbfdf048b48
Ruben Gueunoun <ruben.gueunoun@edhec.com>
2018-12-31 16:33:39 +0100

    api libre

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BaseController < ActionController::API
-  acts_as_token_authentication_handler_for User
+  # acts_as_token_authentication_handler_for User
   include Pundit
 
   after_action :verify_authorized, except: :index
--- app/policies/cagnotte_policy.rb
+++ app/policies/cagnotte_policy.rb
@@ -1,16 +1,17 @@
 class CagnottePolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.where(user: user)
+      # scope.where(user: user)
+      scope.all
     end
   end
 
   def show?
-    record.user == user
+    true
   end
 
   def update?
-    record.user == user
+    true
   end
 
   def create?
