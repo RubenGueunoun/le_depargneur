b2117b588bec2c9c07526d2cfedc8b362ea2d148
Ruben Gueunoun <ruben.gueunoun@edhec.com>
2019-01-01 21:20:03 +0100

    problem prod reglé

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_31_170507) do
+ActiveRecord::Schema.define(version: 2019_01_01_201724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,8 @@ ActiveRecord::Schema.define(version: 2018_12_31_170507) do
     t.string "prenom"
     t.bigint "messenger_id"
     t.string "profil_depargneur"
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
