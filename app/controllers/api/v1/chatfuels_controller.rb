cc07aec392e749e4030aec46b8e7fcb4d1759cd3
Ruben Gueunoun <ruben.gueunoun@edhec.com>
2019-02-26 12:44:44 +0100

    reparation cagnottes

--- app/controllers/api/v1/chatfuels_controller.rb
+++ app/controllers/api/v1/chatfuels_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ChatfuelsController < Api::V1::BaseController
   def relevedecomptes
     user = User.where(messenger_id: params["messenger user id"])
     @cb = CompteBancaire.where(user: user)
-    @cagnottes = Cagnotte.all
+    @cagnottes = Cagnotte.where(user: user)
     @message = ["message"]
     @comptes = []
     @cb.each do |c|
