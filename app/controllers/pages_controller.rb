b80665b78ce8b1e07ea2677441c0860dc3ae34af
Ruben Gueunoun <ruben.gueunoun@edhec.com>
2019-05-22 19:48:19 +0200

    hidden

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,7 @@ class PagesController < ApplicationController
   end
 
   def pre_created
+    redirect_url = "https:%2F%2Fwww.ledepargneur.fr"
+    @url = "https://depargneur.biapi.pro/2.0/auth/webview/connect/select?client_id=#{ENV['BI_CLIENT_ID']}&redirect_uri=#{redirect_url}"
   end
 end
