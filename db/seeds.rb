c3261d2ab191d74cfeb1cd92e9ff524c0f2b2d96
Ruben Gueunoun <ruben.gueunoun@edhec.com>
2019-05-22 19:50:08 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -373,7 +373,7 @@ user1 = User.create!(
   prenom: 'Benjamin',
   email: 'bentoub@gmail.com',
   password: '123456',
-  messenger_id: 20477094785771320,
+  messenger_id: 2047709478577132,
   profil_depargneur: 'normal'
   )
 
