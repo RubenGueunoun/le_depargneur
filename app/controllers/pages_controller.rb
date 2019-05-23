0f3ec1f0ecf8da140441d5c9b025419dfc3ebeb7
Ruben Gueunoun <ruben.gueunoun@edhec.com>
2019-05-23 17:52:05 +0200

    test local

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,14 +14,14 @@ class PagesController < ApplicationController
   end
 
   def pre_created
-    redirect_url = "https:%2F%2Fwww.ledepargneur.fr%2Fconnected_account"
+    redirect_url = "http:%2F%2Flocalhost:3000%2Fconnected_account"
     @url = "https://depargneur.biapi.pro/2.0/auth/webview/connect/select?client_id=#{ENV['BI_CLIENT_ID']}&redirect_uri=#{redirect_url}"
   end
 
   def connected_account
     if params["code"]
       response = BiConnectService.new(params["code"], "/connected_account").call
-      current_user.bi_token = response
+      current_user.bi_token = response["access_token"]
       @valid = true unless response["error"]
       current_user.save
     end
