a5b49c5e72ab569b99707d2396746461826eecf6
Ruben Gueunoun <ruben.gueunoun@edhec.com>
2019-06-02 19:03:51 +0200

    test data bi

--- app/controllers/budgea/webhooks_controller.rb
+++ app/controllers/budgea/webhooks_controller.rb
@@ -1,17 +1,15 @@
-class WebhooksController < ApplicationController
-  before_action :load_user
+class Budgea::WebhooksController < Api::V1::BaseController
 
   def synchronized_data
-    user = @user
+    user = User.find_by(bi_token: request.headers["Authorization"])
+    if user.blank?
+      render json: {}, status: :not_found
+    else
+      render json: {}, status: :ok
+    end
     cb = user.compte_bancaires[0]
-    cb.solde = params[:connections][0][:accounts][0][:balance].to_i
-    cb.save
+    cb.solde = params["connections"][0]["accounts"][0]["balance"]
+    cb.save!
   end
 
-  private
-
-  def load_user
-    @user = User.find_by(bi_token: request.headers["Authorization"])
-    render json: {}, status: "200" unless @user.blank?
-  end
 end
