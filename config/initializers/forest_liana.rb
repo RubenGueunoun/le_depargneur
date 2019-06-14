0c894dbe06052fcb33b4c623ebed670364f4ed22
Ruben Gueunoun <ruben.gueunoun@edhec.com>
2019-06-14 08:51:19 +0300

    forest 2

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,2 +1,2 @@
-ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
-ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
+ForestLiana.env_secret = ENV["FOREST_ENV_SECRET"]
+ForestLiana.auth_secret = ENV["FOREST_AUTH_SECRET"]
